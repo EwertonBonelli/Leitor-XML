a410651131e84e399e3a1e0de31fb3a1dd035630
Ewerton B. Olimpio <ewertonbonelli@hotmail.com>
2021-04-29 23:56:11 -0300

    Pequenas modificacoes e testes

---
+++ Entitties/DadosExcel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClosedXML.Excel; //Biblioteca importada.
+
+namespace Gerenciadorxml.Entitties
+{
+    class DadosExcel
+    {
+        
+    }
+}
